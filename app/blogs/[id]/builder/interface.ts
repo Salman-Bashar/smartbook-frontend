8898c99b769b2d58801b53ed37f7aeb1bb4690d7
Salman Bashar <salman@lemonhive.com>
2025-01-23 21:11:02 +0600

    reactor: remove unused codes

--- app/blogs/[id]/builder/interface.ts
+++
@@ -1,7 +0,0 @@
-import { ISanityCommonPageProps } from '@/backend/sanity/fragments/root/page/interface';
-import { ISanityPortableText } from '@/backend/sanity/fragments/root/portable-text/interface';
-
-export interface ISanityBlogPageQueryResponse extends ISanityCommonPageProps {
-  isFeaturedBlog: boolean;
-  blogContent: ISanityPortableText;
-}
